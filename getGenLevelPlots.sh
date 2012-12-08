eff6a92aa0b5e8e4e266228514205b1aca5fe86b
Nick Edwards <nedwards@cern.ch>
2012-12-08 22:31:45 +0000

    getGenLevelPlots.sh

--- getGenLevelPlots.sh
+++ getGenLevelPlots.sh
@@ -1,2 +1,2 @@
 scp -r ppelx.physics.gla.ac.uk:/nfs/atlas/zz01/ZZJobOutput//ZZ_2011_1Oct/PLOT/GenLevelThesis/Compare20112012 Chapters/TheoryZZProduction/Figures/
-scp -r ppelx.physics.gla.ac.uk:/nfs/atlas/zz01/ZZJobOutput//ZZ_2011_1Oct/PLOT/GenLevelThesis/Compareggqq Chapters/TheoryZZProduction/Figures/
+scp -r ppelx.physics.gla.ac.uk:/nfs/atlas/zz01/ZZJobOutput//ZZ_2011_1Oct/PLOT/GenLevelThesis/Compareggqq7TeV Chapters/TheoryZZProduction/Figures/
