e88062f5415da071d9a1cdc8d44699d37a4d7712
Nick Edwards <nedwards@cern.ch>
2012-12-13 11:57:37 +0000

    getGenLevelPlotsCern.sh

--- getGenLevelPlotsCern.sh
+++ getGenLevelPlotsCern.sh
@@ -1,1 +1,2 @@
 scp -r lxplus405.cern.ch:~/plots/GenLevelThesis/* Chapters/TheoryZZProduction/Figures/GeneratorComparison
+scp -r lxplus405.cern.ch:~/plots/GenLevelThesisQED/* Chapters/TheoryZZProduction/Figures/SherpaQED
