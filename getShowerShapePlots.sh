e0c27e3dbcd2c03b35880fcacf17c113c7444baa
Nick Edwards <nedwards@cern.ch>
2013-01-15 18:26:56 +0000

    getShowerShapePlots.sh

--- getShowerShapePlots.sh
+++ getShowerShapePlots.sh
@@ -1,2 +1,2 @@
-scp -r ppelx.physics.gla.ac.uk:/nfs/atlas/zz01/ZZJobOutput/ShowerShapes8TeV/PLOT/ShowerShapes8TeV/00_lin/ Chapters/Reconstruction/Figures/ShowerShapePlots_lin
+scp -r ppelx.physics.gla.ac.uk:/nfs/atlas/zz01/ZZJobOutput/ShowerShapes8TeV/PLOT/ShowerShapes8TeV/00_withPtCut_lin/ Chapters/Reconstruction/Figures/ShowerShapePlots_lin
 scp -r ppelx.physics.gla.ac.uk:/nfs/atlas/zz01/ZZJobOutput/ShowerShapes8TeV/PLOT/ShowerShapes8TeV/00_log/ Chapters/Reconstruction/Figures/ShowerShapePlots_log
