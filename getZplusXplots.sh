a588ddcd13b6a69cc9ead8674111d7f8cf92b35a
Nick Edwards <nedwards@cern.ch>
2013-01-24 14:38:05 +0000

    getZplusXplots.sh

--- getZplusXplots.sh
+++ getZplusXplots.sh
@@ -1,2 +1,2 @@
 rm -rf Chapters/ObjEventSelection/Figures/ZplusX_log 
-scp -r ppelx.physics.gla.ac.uk:/nfs/atlas/zz01/ZZJobOutput/ZZ7TeV_130119_TwoLepFourLep/PLOT/ZplusXThesis/02_WithHFOR_log Chapters/ObjEventSelection/Figures/ZplusX_log 
+scp -r ppelx.physics.gla.ac.uk:/nfs/atlas/zz01/ZZJobOutput/ZZ7TeV_130119_TwoLepFourLep/PLOT/ZplusXThesis/02_WithHFOR_log_sepProc Chapters/ObjEventSelection/Figures/ZplusX_log 
