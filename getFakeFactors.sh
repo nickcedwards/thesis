ad2c39bf306dab946d268a07c6d7a5862a61f0c2
Nick Edwards <nedwards@cern.ch>
2013-02-20 15:50:14 +0000

    getFakeFactors.sh

---
+++ getFakeFactors.sh
@@ -0,0 +1,2 @@
+rm -rf Chapters/BackgroundEstimate/Figures/FakeFactors
+scp -r ppelx.physics.gla.ac.uk:/nfs/atlas/zz01/ZZJobOutput/ZZ_2011_1Oct/PLOT/FFPlotDataMC/MR05_00 Chapters/BackgroundEstimate/Figures/FakeFactors
