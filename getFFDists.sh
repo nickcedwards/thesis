34a6e82c5bcd90686c3ab945efde7e2362f671b5
Nick Edwards <nedwards@cern.ch>
2013-02-21 18:09:03 +0000

    getFFDists.sh

--- getFFDists.sh
+++ getFFDists.sh
@@ -1,2 +1,3 @@
 rm -rf Chapters/BackgroundEstimate/Figures/ffDists
 scp -r ppelx.physics.gla.ac.uk:/nfs/atlas/zz01/ZZJobOutput/ZZ_2011_1Oct/PLOT/FFDists/MR07_00 Chapters/BackgroundEstimate/Figures/ffDists
+#scp -r ppelx.physics.gla.ac.uk:/nfs/atlas/zz01/ZZJobOutput/ZZ_2011_1Oct/PLOT/FFDists/MR05_00 Chapters/BackgroundEstimate/Figures/ffDists
