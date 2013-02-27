df63b22ccbb2806cb73e278c517db79281966b79
Nick Edwards <nedwards@cern.ch>
2013-02-27 14:05:29 +0000

    getZplusXplots8TeV.sh

---
+++ getZplusXplots8TeV.sh
@@ -0,0 +1,2 @@
+rm -rf Chapters/ObjEventSelection/Figures/ZplusX8TeV_log 
+scp -r ppelx.physics.gla.ac.uk:/nfs/atlas/zz01/ZZJobOutput/ZZ8TeV_130215/PLOT/ZplusXThesis/00_WithHFOR_log_sepProc Chapters/ObjEventSelection/Figures/ZplusX8TeV_log 
