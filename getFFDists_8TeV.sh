59b443324b3dde262b7d0da7fc5993f812a1e1fd
Nick Edwards <nedwards@cern.ch>
2013-02-28 16:42:23 +0000

    getFFDists_8TeV.sh

--- getFFDists_8TeV.sh
+++ getFFDists_8TeV.sh
@@ -1,3 +1,3 @@
-rm -rf Chapters/BackgroundEstimate/Figures/ffDists
-scp -r ppelx.physics.gla.ac.uk:/nfs/atlas/zz01/ZZJobOutput/ZZ8TeV_130215/PLOT/FFDists/MR07_00 Chapters/BackgroundEstimate/Figures/ffDists_8TeV
+rm -rf Chapters/BackgroundEstimate/Figures/ffDists_8TeV
+scp -r ppelx.physics.gla.ac.uk:/nfs/atlas/zz01/ZZJobOutput/ZZ8TeV_130215/PLOT/FFDists/MR08 Chapters/BackgroundEstimate/Figures/ffDists_8TeV
 #scp -r ppelx.physics.gla.ac.uk:/nfs/atlas/zz01/ZZJobOutput/ZZ_2011_1Oct/PLOT/FFDists/MR05_00 Chapters/BackgroundEstimate/Figures/ffDists
