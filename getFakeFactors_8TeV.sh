5e10d88d67625cfa3cbc2882e61abc848451883b
Nick Edwards <nedwards@cern.ch>
2013-02-28 16:49:15 +0000

    getFakeFactors_8TeV.sh

--- getFakeFactors_8TeV.sh
+++ getFakeFactors_8TeV.sh
@@ -1,3 +1,3 @@
 rm -rf Chapters/BackgroundEstimate/Figures/FakeFactors8TeV
-scp -r ppelx.physics.gla.ac.uk:/nfs/atlas/zz01/ZZJobOutput/ZZ8TeV_20130215/PLOT/FFPlotDataMC/MR08_00 Chapters/BackgroundEstimate/Figures/FakeFactors8TeV
+scp -r ppelx.physics.gla.ac.uk:/nfs/atlas/zz01/ZZJobOutput/ZZ8TeV_130215/PLOT/FFPlotDataMC/MR08_00 Chapters/BackgroundEstimate/Figures/FakeFactors8TeV
 #scp -r ppelx.physics.gla.ac.uk:/nfs/atlas/zz01/ZZJobOutput/ZZ_2011_1Oct/PLOT/FFPlotDataMC/MR05_00 Chapters/BackgroundEstimate/Figures/FakeFactors
