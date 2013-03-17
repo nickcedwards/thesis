d1b33ad317080bd854c07f16c6195f7d7211cfbe
Nick Edwards <nedwards@cern.ch>
2013-03-17 13:45:32 +0000

    getDilepPlots8TeV.sh

--- getDilepPlots8TeV.sh
+++ getDilepPlots8TeV.sh
@@ -1,3 +1,3 @@
 rm -rf Chapters/ObjEventSelection/Figures/Dilepton8TeV 
-scp -r ppelx.physics.gla.ac.uk:/nfs/atlas/zz01/ZZJobOutput/ZZ8TeV_130215/PLOT/DilepThesis_NoRatio/02_WithHFOR_withCaloIso Chapters/ObjEventSelection/Figures/Dilepton8TeV
+scp -r ppelx.physics.gla.ac.uk:/nfs/atlas/zz01/ZZJobOutput/ZZ8TeV_130215/PLOT/DilepThesis_NoRatio/02_WithHFOR_withCaloIso_log Chapters/ObjEventSelection/Figures/Dilepton8TeV
 #scp -r ppelx.physics.gla.ac.uk:/nfs/atlas/zz01/ZZJobOutput/ZZ8TeV_130215/PLOT/DilepThesis_NoRatio/01_WithHFOR_log Chapters/ObjEventSelection/Figures/Dilepton8TeV
