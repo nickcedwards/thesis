6d80ac53db43021c76687f22ac30482a5984a1f5
Nick Edwards <nedwards@cern.ch>
2013-03-17 20:23:45 +0000

    CalculateTotalBg.py

---
+++ CalculateTotalBg.py
@@ -0,0 +1,12 @@
+import AddInQuad
+
+channels = ["EEEE", "EEMM", "MMMM", "LLLL"]
+
+# central stat_up, stat_down, sys
+dd_bg_est = {
+        "Seven" : {
+            "ZZ" : {
+"EEEE" : [ 0.3, 0.6, 0.5, 0.1],
+"MMMM" <0.7}}
+"EEMM" : [ 0.5 ,1.2,0.8,0.3],
+"LLLL" : [ 0.2 ,1.3,1.1,0.4],
