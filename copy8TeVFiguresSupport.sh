2f1e5446d2d6494df4ea886b6095771c36648b1c
Nick Edwards <nedwards@cern.ch>
2013-03-18 11:57:39 +0000

    copy8TeVFiguresSupport.sh

--- copy8TeVFiguresSupport.sh
+++ copy8TeVFiguresSupport.sh
@@ -1,1 +1,1 @@
-cp ~/physics/ZZ_Winter2013/SupportDocument/trunk/figures/ZZ4l_KinematicPlots/*eps Chapters/CrossSection/Figures/8TeV/
+cp ~/physics/ZZ_Winter2013/ZZ4l/SupportDocument/trunk/figures/ZZ4l_KinematicPlots/*eps Chapters/CrossSection/Figures/8TeV/
