108faf5c321ef046c1700f8983ecf1dddef0d74d
Nick Edwards <nedwards@cern.ch>
2013-03-20 16:30:32 +0000

    getDilepPlots.sh

--- getDilepPlots.sh
+++ getDilepPlots.sh
@@ -1,2 +1,3 @@
 rm -rf Chapters/ObjEventSelection/Figures/Dilepton7TeV 
-scp -r ppelx.physics.gla.ac.uk:/nfs/atlas/zz01/ZZJobOutput/ZZ7TeV_130124_TwoLepFourLep/PLOT/DilepThesis_AlpgenWg/02_WithHFOR_withTriggerMatchPtCut_log Chapters/ObjEventSelection/Figures/Dilepton7TeV
+scp -r ppelx.physics.gla.ac.uk:/nfs/atlas/zz01/ZZJobOutput/ZZ7TeV_130124_TwoLepFourLep/PLOT/DilepThesis_HerwigWZ/02_WithHFOR_withTriggerMatchPtCut_log Chapters/ObjEventSelection/Figures/Dilepton7TeV
+#scp -r ppelx.physics.gla.ac.uk:/nfs/atlas/zz01/ZZJobOutput/ZZ7TeV_130124_TwoLepFourLep/PLOT/DilepThesis_AlpgenWg/02_WithHFOR_withTriggerMatchPtCut_log Chapters/ObjEventSelection/Figures/Dilepton7TeV
