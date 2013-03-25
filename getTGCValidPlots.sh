b31c52deb047cb1eeaa01ae6eaf97b32945d64ab
Nick Edwards <nedwards@cern.ch>
2013-03-25 14:46:56 +0000

    getTGCValidPlots.sh

--- getTGCValidPlots.sh
+++ getTGCValidPlots.sh
@@ -2,4 +2,6 @@ rm -rf Chapters/TGCLimits/Figures/ReweightValidation/04_BR
 scp -r ppelx.physics.gla.ac.uk:/nfs/atlas/zz01/ZZJobOutput/ZZ8TeV_130122_CzzCutflows/PLOT/TGCHist_Validation/04_BR Chapters/TGCLimits/Figures/ReweightValidation/04_BR
 rm -rf Chapters/TGCLimits/Figures/ReweightValidation/04_BHO_noFF
 scp -r ppelx.physics.gla.ac.uk:/nfs/atlas/zz01/ZZJobOutput/ZZ8TeV_130122_CzzCutflows/PLOT/TGCHist_Validation/04_BHO_noFF Chapters/TGCLimits/Figures/ReweightValidation/04_BHO_noFF
+rm -rf Chapters/TGCLimits/Figures/TGCHist_At7TeV/04_BR
+scp -r ppelx.physics.gla.ac.uk:/nfs/atlas/zz01/ZZJobOutput/ZZ8TeV_130122_CzzCutflows/PLOT/TGCHist_At7TeV/04_BR Chapters/TGCLimits/Figures/ReweightAt7TeVLimits
 #scp -r ppelx.physics.gla.ac.uk:/nfs/atlas/zz01/ZZJobOutput/ZZ7TeV_130124_TwoLepFourLep/PLOT/DilepThesis_AlpgenWg/02_WithHFOR_withTriggerMatchPtCut_log Chapters/ObjEventSelection/Figures/Dilepton7TeV
