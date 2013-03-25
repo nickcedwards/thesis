8a5a59d82b6743d3ddd2db0ad5fabfc12ef08e0e
Nick Edwards <nedwards@cern.ch>
2013-03-25 12:23:28 +0000

    getTGCValidPlots.sh

---
+++ getTGCValidPlots.sh
@@ -0,0 +1,3 @@
+#rm -rf Chapters/TGCLimits/Figures/ReweightValidation 
+scp -r ppelx.physics.gla.ac.uk:/nfs/atlas/zz01/ZZJobOutput/ZZ8TeV_130122_CzzCutflows/PLOT/TGCHist_Validation/04_BR/*eps Chapters/TGCLimits/Figures/ReweightValidation/
+#scp -r ppelx.physics.gla.ac.uk:/nfs/atlas/zz01/ZZJobOutput/ZZ7TeV_130124_TwoLepFourLep/PLOT/DilepThesis_AlpgenWg/02_WithHFOR_withTriggerMatchPtCut_log Chapters/ObjEventSelection/Figures/Dilepton7TeV
