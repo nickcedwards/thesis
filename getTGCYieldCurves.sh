e43d23243c4fb298e6fb5258ef20f0710249ca9d
Nick Edwards <nedwards@cern.ch>
2013-03-25 16:57:11 +0000

    getTGCYieldCurves.sh

---
+++ getTGCYieldCurves.sh
@@ -0,0 +1,3 @@
+rm -rf Chapters/TGCLimits/Figures/TGCYieldCoeffs
+scp -r ppelx.physics.gla.ac.uk:/nfs/atlas/zz01/ZZJobOutput/ZZ8TeV_130122_CzzCutflows/PLOT/TGCYieldCoeffs/04_BR Chapters/TGCLimits/Figures/YieldCoeffs
+#scp -r ppelx.physics.gla.ac.uk:/nfs/atlas/zz01/ZZJobOutput/ZZ7TeV_130124_TwoLepFourLep/PLOT/DilepThesis_AlpgenWg/02_WithHFOR_withTriggerMatchPtCut_log Chapters/ObjEventSelection/Figures/Dilepton7TeV
