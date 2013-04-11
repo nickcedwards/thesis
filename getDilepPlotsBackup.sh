2f85654585e64f78f181af6b9ab50146ceef6f83
Nick Edwards <nedwards@cern.ch>
2013-04-11 16:20:32 +0100

    getDilepPlotsBackup.sh

---
+++ getDilepPlotsBackup.sh
@@ -0,0 +1,5 @@
+rm -rf Chapters/Backup/Figures/Dilepton7TeVRatio
+scp -r ppelx.physics.gla.ac.uk:/nfs/atlas/zz01/ZZJobOutput/ZZ7TeV_130124_TwoLepFourLep/PLOT/DilepThesis_HerwigWZ/02_WithHFOR_withTriggerMatchPtCut_ratio_log Chapters/Backup/Figures/Dilepton7TeVRatio
+rm -rf Chapters/Backup/Figures/Dilepton8TeVRatio
+scp -r ppelx.physics.gla.ac.uk:/nfs/atlas/zz01/ZZJobOutput/ZZ8TeV_130215/PLOT/DilepThesis_Ratio/03_WithHFOR_withCaloIso_log Chapters/ObjEventSelection/Figures/Dilepton8TeVRatio
+#scp -r ppelx.physics.gla.ac.uk:/nfs/atlas/zz01/ZZJobOutput/ZZ8TeV_130215/PLOT/DilepThesis_NoRatio/01_WithHFOR_log Chapters/ObjEventSelection/Figures/Dilepton8TeV
