febf5bf16ce0727d95ded99849527f7f8e2e44e0
Nick Edwards <nedwards@cern.ch>
2013-04-11 16:51:01 +0100

    getSignalPlotsIrred.sh

---
+++ getSignalPlotsIrred.sh
@@ -0,0 +1,2 @@
+rm -rf Chapters/Backup/Figures/SignalPlotsIrred
+scp -r ppelx.physics.gla.ac.uk:/nfs/atlas/zz01/ZZJobOutput/ZZ8TeV_130226/PLOT/GridOutput/8TeV_Note_130218_withBgSyst_withIred_lin Chapters/Backup/Figures/SignalPlotsIrred
